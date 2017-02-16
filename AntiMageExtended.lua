66ebaa8a3b05a0b55ca066d235c759a0819daec7
Tao Li <eroicacmcs@gmail.com>
2017-02-16 01:52:13 -0800

    am update

--- AntiMageExtended.lua
+++ AntiMageExtended.lua
@@ -1,8 +1,9 @@
 local AntiMageExtended = {}
 
 AntiMageExtended.optionEnabled = Menu.AddOption({"Hero Specific","Anti-Mage"},"Auto Mana Void 2", "Extended Auto Mana Void")
+AntiMageExtended.font = Renderer.LoadFont("Tahoma", 30, Enum.FontWeight.EXTRABOLD)
 
-function AntiMageExtended.OnUpdate()
+function AntiMageExtended.OnDraw()
 	if not Menu.IsEnabled(AntiMageExtended.optionEnabled) then return end
 	local myHero = Heroes.GetLocal()
 	if (not myHero) or NPC.GetUnitName(myHero) ~= "npc_dota_hero_antimage" then return end
@@ -13,11 +14,11 @@ function AntiMageExtended.OnUpdate()
 	local damageManaVoid = Ability.GetLevelSpecialValueFor(skillManaVoid, "mana_void_damage_per_mana")
 
 	unitsAround = NPC.GetHeroesInRadius(myHero, rangeManaVoid, Enum.TeamType.TEAM_ENEMY)
-	if not Ability.IsCastable(skillManaVoid, myMana) then return end
 
 	local maxManaDiff = 0
 	local maxManaDiffEnemy = nil
 	local leastHealth = 100000
+	local damage = 0
 	
 	for i,enemy in ipairs(unitsAround) do
 		local manaDiff = NPC.GetMaxMana(enemy) - NPC.GetMana(enemy)
@@ -31,10 +32,17 @@ function AntiMageExtended.OnUpdate()
 			leastHealth = enemyHealth
 		end
 
+		damage = damageManaVoid * maxManaDiff * (1 - NPC.GetMagicalArmorValue(maxManaDiffEnemy))
+		local hitDamage = NPC.GetDamageMultiplierVersus(myHero, enemy) * (NPC.GetTrueDamage(myHero) * NPC.GetArmorDamageMultiplier(enemy))
+		local hitsLeft = math.ceil((enemyHealth - damage) / hitDamage)
+		local pos = NPC.GetAbsOrigin(enemy)
+		local x, y, visible = Renderer.WorldToScreen(pos)
+		Renderer.SetDrawColor(255, 255, 0, 255)
+		Renderer.DrawTextCentered(AntiMageExtended.font, x, y, hitsLeft, 1)	
+
 	end
 
-	local damage = damageManaVoid * maxManaDiff * (1 - NPC.GetMagicalArmorValue(maxManaDiffEnemy))
-	if damage >= leastHealth then
+	if damage >= leastHealth and Ability.IsCastable(skillManaVoid, myMana) then
 		Ability.CastTarget(skillManaVoid, maxManaDiffEnemy, true)
 	end
 
