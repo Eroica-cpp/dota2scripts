f4ada545bde7a417307d262edaebdb11084d726e
Tao Li <eroicacmcs@gmail.com>
2017-02-17 01:48:05 -0800

    phoenix auto cast fire spirit

---
+++ Phoenix.lua
@@ -0,0 +1,31 @@
+local Phoenix = {}
+
+Phoenix.optionEnabled = Menu.AddOption({"Hero Specific","Phoenix"},"Auto Fire Spirit", "auto cast fire spirit when enabled")
+
+function Phoenix.OnUpdate()
+
+	if not Menu.IsEnabled(Phoenix.optionEnabled) then return end
+	local myHero = Heroes.GetLocal()
+	if not myHero then return end
+	if NPC.GetUnitName(myHero) ~= "npc_dota_hero_phoenix" then return end
+	local myMana = NPC.GetMana(myHero)
+
+	local dive = NPC.GetAbilityByIndex(myHero, 0)
+	local fireSpirit = NPC.GetAbilityByIndex(myHero, 1)
+	local fireSpiritLevel = Ability.GetLevel(fireSpirit)
+
+	if fireSpiritLevel <= 0 or (not Ability.IsInAbilityPhase(dive)) then return end
+
+	local radius = Ability.GetCastRange(dive) + Ability.GetCastRange(fireSpirit)
+	local unitsAround = NPC.GetHeroesInRadius(myHero, radius, Enum.TeamType.TEAM_ENEMY)
+
+	for i, enemy in ipairs(unitsAround) do
+		local enemyPos = NPC.GetAbsOrigin(enemy)
+		if Ability.IsCastable(fireSpirit, myMana) then
+			Ability.CastPosition(fireSpirit, enemyPos)
+		end
+	end
+
+end
+
+return Phoenix
