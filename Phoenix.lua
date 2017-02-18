433d25657b795f805e4a1cc6b1c05562f7d89040
Tao Li <eroicacmcs@gmail.com>
2017-02-17 16:58:56 -0800

    use diff function to get enemy heroes that was in fog at the beginning

--- Phoenix.lua
+++ Phoenix.lua
@@ -1,15 +1,13 @@
 -- ==================================
 -- File Name : Phoenix.lua
 -- Author    : Eroica
--- Version   : 1.0
+-- Version   : 1.1
 -- Date      : 2017.2.17
 -- ==================================
 
 local Phoenix = {}
 
-Phoenix.optionEnabled = Menu.AddOption({"Hero Specific","Phoenix"},"Auto Fire Spirit", "auto cast fire spirit when enabled")
-
-enemyPosList = {}
+Phoenix.optionEnabled = Menu.AddOption({"Hero Specific","Phoenix"},"Auto Fire Spirit", "auto cast fire spirit while diving if enabled")
 
 function Phoenix.OnUpdate()
 
@@ -18,22 +16,28 @@ function Phoenix.OnUpdate()
 	if not myHero then return end
 	if NPC.GetUnitName(myHero) ~= "npc_dota_hero_phoenix" then return end
 	local myMana = NPC.GetMana(myHero)
+	local myTeam = Entity.GetTeamNum(myHero)
 
 	local dive = NPC.GetAbilityByIndex(myHero, 0)
 	local fireSpirit = NPC.GetAbilityByIndex(myHero, 1)
 	local fireSpiritLevel = Ability.GetLevel(fireSpirit)
-	local radius = 3000
 
 	if fireSpiritLevel <= 0 or (not Ability.IsInAbilityPhase(dive)) then return end
 
-	local unitsAround = NPC.GetHeroesInRadius(myHero, radius, Enum.TeamType.TEAM_ENEMY)
+	for i = 1, Heroes.Count() do
+		
+		local npc = Heroes.Get(i)
+		
+		if (not NPC.IsIllusion(npc)) and not (Entity.GetTeamNum(npc) == myTeam) then
+			
+			local enemyPos = NPC.GetAbsOrigin(npc)
+			
+			if Ability.IsCastable(fireSpirit, myMana) then
+				Ability.CastPosition(fireSpirit, enemyPos)
+			end
 
-	for i, enemy in ipairs(unitsAround) do
-		local enemyPos = NPC.GetAbsOrigin(enemy)
-		if Ability.IsCastable(fireSpirit, myMana) and not enemyPosList[enemyPos] then
-			Ability.CastPosition(fireSpirit, enemyPos)
-			enemyPosList[enemyPos] = true
 		end
+
 	end
 
 end
