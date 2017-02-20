d2495d86464dddee7a034eb79fae25a07d9ce505
Tao Li <eroicacmcs@gmail.com>
2017-02-20 03:38:01 -0800

    update db

--- ItemCooldown.lua
+++ ItemCooldown.lua
@@ -3,50 +3,50 @@
 local ItemCooldown = {}
 
 ItemCooldown.optionEnable = Menu.AddOption({ "Awareness" }, "Item Cooldown Display", "Show items cooldown")
+ItemCooldown.font = Renderer.LoadFont("Tahoma", 15, Enum.FontWeight.EXTRABOLD)
 
 -- important item database
 ItemCooldown.importantItems = {}
-ItemCooldown.importantItems["item_dagon"] = true -- "Dagon 1"
-ItemCooldown.importantItems["item_dagon_2"] = true -- "Dagon 2"
-ItemCooldown.importantItems["item_dagon_3"] = true -- "Dagon 3"
-ItemCooldown.importantItems["item_dagon_4"] = true -- "Dagon 4"
-ItemCooldown.importantItems["item_dagon_5"] = true -- "Dagon 5"
-ItemCooldown.importantItems["item_aegis"] = true --"Aegis of the Immortal"
-ItemCooldown.importantItems["item_blink"] = true -- "Blink Dagger"
-ItemCooldown.importantItems["item_cheese"] = true -- "Cheese"
-ItemCooldown.importantItems["item_dust"] = true -- "Dust of Appearance"
-ItemCooldown.importantItems["item_faerie_fire"] = true -- "Faerie Fire"
-ItemCooldown.importantItems["item_gem"] = true -- "Gem of True Sight"
-ItemCooldown.importantItems["item_ghost"] = true -- "Ghost Scepter"
-ItemCooldown.importantItems["item_flask"] = true -- "Healing Salve"
-ItemCooldown.importantItems["item_infused_raindrop"] = true -- "Infused Raindrop"
-ItemCooldown.importantItems["item_shadow_amulet"] = true -- "Shadow Amulet"
-ItemCooldown.importantItems["item_smoke_of_deceit"] = true -- "Smoke of Deceit"
-ItemCooldown.importantItems["item_abyssal_blade"] = true -- "Abyssal Blade"
-ItemCooldown.importantItems["item_black_king_bar"] = true -- "Black King Bar"
-ItemCooldown.importantItems["item_blade_mail"] = true -- "Blade Mail"
-ItemCooldown.importantItems["item_tpscroll"] = true -- "Town Portal Scroll"
-ItemCooldown.importantItems["item_travel_boots"] = true -- "Boots of Trave 1"
-ItemCooldown.importantItems["item_travel_boots_2"] = true -- "Boots of Trave 2"
-ItemCooldown.importantItems["item_ethereal_blade"] = true -- "Ethereal Blade"
-ItemCooldown.importantItems["item_cyclone"] = true -- "Eul's Scepter of Divinity"
-ItemCooldown.importantItems["item_skadi"] = true -- "Eye of Skadi"
-ItemCooldown.importantItems["item_force_staff"] = true -- "Force Staff"
-ItemCooldown.importantItems["item_glimmer_cape"] = true -- "Glimmer Cape"
-ItemCooldown.importantItems["item_guardian_greaves"] = true -- "Guardian Greaves"
-ItemCooldown.importantItems["item_heavens_halberd"] = true -- "Heaven's Halberd"
-ItemCooldown.importantItems["item_hurricane_pike"] = true -- "Hurricane Pike"
-ItemCooldown.importantItems["item_sphere"] = true -- "Linken's Sphere"
-ItemCooldown.importantItems["item_manta"] = true -- "Manta Style"
-ItemCooldown.importantItems["item_mekansm"] = true -- "Mekansm"
-ItemCooldown.importantItems["item_orchid"] = true -- "Orchid Malevolence"
-ItemCooldown.importantItems["item_rapier"] = true -- "Divine Rapier"
-ItemCooldown.importantItems["item_refresher"] = true -- "Refresher Orb"
-ItemCooldown.importantItems["item_rod_of_atos"] = true -- "Rod of Atos"
-ItemCooldown.importantItems["item_satanic"] = true -- "Satanic"
-ItemCooldown.importantItems["item_sheepstick"] = true -- "Scythe of Vyse"
-ItemCooldown.importantItems["item_invis_sword"] = true -- "Shadow Blade"
-ItemCooldown.importantItems["item_silver_edge"] = true -- "Silver Edge"
+ItemCooldown.importantItems["item_dagon"] = "dagon" -- "Dagon 1"
+ItemCooldown.importantItems["item_dagon_2"] = "dagon" -- "Dagon 2"
+ItemCooldown.importantItems["item_dagon_3"] = "dagon" -- "Dagon 3"
+ItemCooldown.importantItems["item_dagon_4"] = "dagon" -- "Dagon 4"
+ItemCooldown.importantItems["item_dagon_5"] = "dagon" -- "Dagon 5"
+ItemCooldown.importantItems["item_aegis"] = "aegis" --"Aegis of the Immortal"
+ItemCooldown.importantItems["item_blink"] = "blink" -- "Blink Dagger"
+ItemCooldown.importantItems["item_cheese"] = "cheese" -- "Cheese"
+ItemCooldown.importantItems["item_dust"] = "dust" -- "Dust of Appearance"
+ItemCooldown.importantItems["item_faerie_fire"] = "faerie_fire" -- "Faerie Fire"
+ItemCooldown.importantItems["item_gem"] = "gem" -- "Gem of True Sight"
+ItemCooldown.importantItems["item_ghost"] = "ghost_scepter" -- "Ghost Scepter"
+ItemCooldown.importantItems["item_flask"] = "salve" -- "Healing Salve"
+ItemCooldown.importantItems["item_infused_raindrop"] = "raindrop" -- "Infused Raindrop"
+ItemCooldown.importantItems["item_shadow_amulet"] = "shadow_amulet" -- "Shadow Amulet"
+ItemCooldown.importantItems["item_smoke_of_deceit"] = "smoke" -- "Smoke of Deceit"
+ItemCooldown.importantItems["item_abyssal_blade"] = "abyssal_blade" -- "Abyssal Blade"
+ItemCooldown.importantItems["item_black_king_bar"] = "BKB" -- "Black King Bar"
+ItemCooldown.importantItems["item_blade_mail"] = "blade_mail" -- "Blade Mail"
+ItemCooldown.importantItems["item_tpscroll"] = "tp" -- "Town Portal Scroll"
+ItemCooldown.importantItems["item_travel_boots"] = "tp_boot" -- "Boots of Trave 1"
+ItemCooldown.importantItems["item_travel_boots_2"] = "tp_boot" -- "Boots of Trave 2"
+ItemCooldown.importantItems["item_ethereal_blade"] = "ethereal_blade" -- "Ethereal Blade"
+ItemCooldown.importantItems["item_cyclone"] = "eul" -- "Eul's Scepter of Divinity"
+ItemCooldown.importantItems["item_force_staff"] = "force_staff" -- "Force Staff"
+ItemCooldown.importantItems["item_glimmer_cape"] = "glimmer_cape" -- "Glimmer Cape"
+ItemCooldown.importantItems["item_guardian_greaves"] = "guardian_greaves" -- "Guardian Greaves"
+ItemCooldown.importantItems["item_heavens_halberd"] = "heavens_halberd" -- "Heaven's Halberd"
+ItemCooldown.importantItems["item_hurricane_pike"] = "hurricane_pike" -- "Hurricane Pike"
+ItemCooldown.importantItems["item_sphere"] = "linken" -- "Linken's Sphere"
+ItemCooldown.importantItems["item_manta"] = "manta" -- "Manta Style"
+ItemCooldown.importantItems["item_mekansm"] = "mekansm" -- "Mekansm"
+ItemCooldown.importantItems["item_orchid"] = "orchid" -- "Orchid Malevolence"
+ItemCooldown.importantItems["item_rapier"] = "rapier" -- "Divine Rapier"
+ItemCooldown.importantItems["item_refresher"] = "refresher" -- "Refresher Orb"
+ItemCooldown.importantItems["item_rod_of_atos"] = "atos" -- "Rod of Atos"
+ItemCooldown.importantItems["item_satanic"] = "satanic" -- "Satanic"
+ItemCooldown.importantItems["item_sheepstick"] = "sheepstick" -- "Scythe of Vyse"
+ItemCooldown.importantItems["item_invis_sword"] = "shadow_blade" -- "Shadow Blade"
+ItemCooldown.importantItems["item_silver_edge"] = "silver_edge" -- "Silver Edge"
 
 function ItemCooldown.OnDraw()
 	if not Menu.IsEnabled(ItemCooldown.optionEnable) then return end
@@ -54,15 +54,49 @@ function ItemCooldown.OnDraw()
 	local myHero = Heroes.GetLocal()
 	if not myHero then return end
 
-	local slotNum = 9
-	for i = 0, slotNum-1 do
-		myItem = NPC.GetItemByIndex(myHero, i)
-		if myItem then
-		    myItemCD = Ability.GetCooldown(myItem)
-			Log.Write(i+1 .. ": " .. myItemCD)
+	for i = 1, Heroes.Count() do
+		local hero = Heroes.Get(i)
+		if not Entity.IsSameTeam(myHero, hero) and not Entity.IsDormant(hero) and not NPC.IsIllusion(hero) and Entity.IsAlive(hero) then
+			ItemCooldown.DrawDisplay(hero)
 		end
 	end
 
+end
+
+function ItemCooldown.DrawDisplay(hero)
+
+	local slotNum = 9
+	-- draw para
+	local pos = NPC.GetAbsOrigin(hero)
+	local x, y, visible = Renderer.WorldToScreen(pos)
+	local gapX = 30
+	local gapY = 20
+
+	for i = 0, slotNum-1 do
+		local item = NPC.GetItemByIndex(hero, i)
+		
+		if item then
+			local itemName = Ability.GetName(item)
+	
+			if ItemCooldown.importantItems[itemName] then
+				local itemCd = math.ceil(Ability.GetCooldown(item))
+
+				-- draw
+				if itemCd > 0 then
+					Renderer.SetDrawColor(255, 0, 0, 255)
+					local text = itemName.."("..itemCd..")"
+					Renderer.DrawTextCentered(ItemCooldown.font, x+gapX, y, text, 1)
+				else
+					Renderer.SetDrawColor(0, 255, 0, 255)
+					Renderer.DrawTextCentered(ItemCooldown.font, x+gapX, y, itemName, 1)
+				end
+				y = y + gapY
+
+			end
+	
+		end
+	
+	end -- end of big for loop
 
 end
 
