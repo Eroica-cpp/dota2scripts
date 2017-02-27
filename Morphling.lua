e713100c7aa987d04842660e898dba04c436c326
Tao Li <eroicacmcs@gmail.com>
2017-02-26 18:49:27 -0800

    add auto cast strike for KS

--- Morphling.lua
+++ Morphling.lua
@@ -6,7 +6,7 @@ Morphling.font = Renderer.LoadFont("Tahoma", 30, Enum.FontWeight.EXTRABOLD)
 
 local magicDamageFactor = 0.75
 
-function Morphling.OnUpdate()
+function Morphling.OnDraw()
 	local myHero = Heroes.GetLocal()
 	if not myHero then return end
 	if NPC.GetUnitName(myHero) ~= "npc_dota_hero_morphling" then return end
@@ -17,13 +17,33 @@ function Morphling.OnUpdate()
 end
 
 function Morphling.Awareness(myHero)
-	local strikeDmg = getStrikeDamamge(myHero)
-	Log.Write("strikeDmg: " .. strikeDmg*magicDamageFactor)
+	local myMana = NPC.GetMana(myHero)
+	local strike = NPC.GetAbilityByIndex(myHero, 1)
+	local strikeCastRange = Ability.GetCastRange(strike)
+	local strikeDamage = getStrikeDamamge(myHero) * magicDamageFactor
 	
 	for i = 1, Heroes.Count() do
 		local enemy = Heroes.Get(i)
 		if not NPC.IsIllusion(enemy) and not Entity.IsSameTeam(myHero, enemy) and not Entity.IsDormant(enemy) and Entity.IsAlive(enemy) then
 			local physicalDamage = NPC.GetDamageMultiplierVersus(myHero, enemy) * NPC.GetTrueDamage(myHero) * NPC.GetArmorDamageMultiplier(enemy)
+			local enemyHp = Entity.GetHealth(enemy)
+			local enemyHpLeft = enemyHp - strikeDamage
+			local hitsLeft = math.ceil(enemyHpLeft / physicalDamage)
+
+			local pos = NPC.GetAbsOrigin(enemy)
+			local x, y, visible = Renderer.WorldToScreen(pos)
+
+			-- red : can kill; green : cant kill
+			if enemyHpLeft <= 0 then
+				if strike and Ability.IsCastable(strike, myMana) and NPC.IsEntityInRange(enemy, myHero, strikeCastRange) and not NPC.HasState(enemy, Enum.ModifierState.MODIFIER_STATE_MAGIC_IMMUNE) then
+					Ability.CastTarget(strike, enemy)
+				end
+				Renderer.SetDrawColor(255, 0, 0, 255)
+				Renderer.DrawTextCentered(Morphling.font, x, y, "Kill", 1)
+			else
+				Renderer.SetDrawColor(0, 255, 0, 255)
+				Renderer.DrawTextCentered(Morphling.font, x, y, hitsLeft, 1)
+			end
 
 		end -- end of if statement
 	end -- enf of for loop
@@ -50,7 +70,6 @@ function getStrikeDamamge(myHero)
 	multiplier = multiplier > maxMultiplier and maxMultiplier or multiplier
 	multiplier = multiplier < minMultiplier and minMultiplier or multiplier
 
-	Log.Write("multiplier: " .. multiplier)
 	return basicDamage + myAgility * multiplier
 end
 
