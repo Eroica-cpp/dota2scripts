6a41a848241dc7cca79db05af6703a4e63de65f3
Tao Li <eroicacmcs@gmail.com>
2017-02-27 02:12:47 -0800

    consider wave's damage

--- Morphling.lua
+++ Morphling.lua
@@ -16,6 +16,11 @@ end
 
 function Morphling.Awareness(myHero)
 	local myMana = NPC.GetMana(myHero)
+
+	local wave = NPC.GetAbilityByIndex(myHero, 0)
+	local waveLevel = Ability.GetLevel(wave)
+	local waveDamage = (waveLevel > 0) and 100+75*(waveLevel-1) or 0
+
 	local strike = NPC.GetAbilityByIndex(myHero, 1)
 	local strikeCastRange = Ability.GetCastRange(strike)
 	local strikeDamage = getStrikeDamamge(myHero)
@@ -24,6 +29,7 @@ function Morphling.Awareness(myHero)
 	local etherealCastRange = ethereal and Ability.GetCastRange(ethereal) or 0
 	local etherealDamge = getEtherealDamage(myHero, ethereal)
 
+	if not wave or not Ability.IsCastable(wave, myMana) then waveDamage = 0 end
 	if not strike or not Ability.IsCastable(strike, myMana) then strikeDamage = 0 end
 	if not ethereal or not Ability.IsCastable(ethereal, myMana) then etherealDamge = 0 end
 
@@ -31,28 +37,33 @@ function Morphling.Awareness(myHero)
 		local enemy = Heroes.Get(i)
 		if not NPC.IsIllusion(enemy) and not Entity.IsSameTeam(myHero, enemy) and not Entity.IsDormant(enemy) and Entity.IsAlive(enemy) then
 			local physicalDamage = NPC.GetDamageMultiplierVersus(myHero, enemy) * NPC.GetTrueDamage(myHero) * NPC.GetArmorDamageMultiplier(enemy)
+			local trueWaveDamage = waveDamage * NPC.GetMagicalArmorDamageMultiplier(enemy)
 			local trueStrikeDamage = strikeDamage * NPC.GetMagicalArmorDamageMultiplier(enemy)
 			local trueEtherealDamage = etherealDamge * NPC.GetMagicalArmorDamageMultiplier(enemy)
 
+			-- didnt consider amplification of wave's damage
 			if ethereal and Ability.IsCastable(ethereal, myMana) then
 				trueEtherealDamage = 1.4 * trueEtherealDamage
 			end
 
 			local enemyHp = Entity.GetHealth(enemy)
-			local enemyHpLeft = enemyHp - trueStrikeDamage - trueEtherealDamage
+			local enemyHpLeft = enemyHp - trueStrikeDamage - trueEtherealDamage - trueWaveDamage
 			local hitsLeft = math.ceil(enemyHpLeft / physicalDamage)
 
 			local pos = NPC.GetAbsOrigin(enemy)
 			local x, y, visible = Renderer.WorldToScreen(pos)
 
 			-- red : can kill; green : cant kill
-			if enemyHpLeft <= 0 then
+			if enemyHp - trueStrikeDamage - trueEtherealDamage <= 0 then
 				if ethereal and Ability.IsCastable(ethereal, myMana) and NPC.IsEntityInRange(enemy, myHero, etherealCastRange) and not NPC.HasState(enemy, Enum.ModifierState.MODIFIER_STATE_MAGIC_IMMUNE) then
 					Ability.CastTarget(ethereal, enemy)
 				end
 				if strike and Ability.IsCastable(strike, myMana) and NPC.IsEntityInRange(enemy, myHero, strikeCastRange) and not NPC.HasState(enemy, Enum.ModifierState.MODIFIER_STATE_MAGIC_IMMUNE) then
 					Ability.CastTarget(strike, enemy)
 				end
+				Renderer.SetDrawColor(255, 0, 0, 255)
+				Renderer.DrawTextCentered(Morphling.font, x, y, "Kill", 1)
+			elseif enemyHpLeft <= 0 then
 				Renderer.SetDrawColor(255, 0, 0, 255)
 				Renderer.DrawTextCentered(Morphling.font, x, y, "Kill", 1)
 			else
