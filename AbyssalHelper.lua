cada2e71769362df2e9b61ccd9175a4c6c55eae8
Tao Li <eroicacmcs@gmail.com>
2017-03-14 12:52:32 -0700

    ok version for blink+abyssal combo

--- AbyssalHelper.lua
+++ AbyssalHelper.lua
@@ -4,11 +4,24 @@ AbyssalHelper.option = Menu.AddOption({"Utility", "Abyssal Blade Helper"}, "Use 
 
 function AbyssalHelper.OnPrepareUnitOrders(orders)
 	if not Menu.IsEnabled(AbyssalHelper.option) then return true end
-	if not orders or not orders.npc or not orders.ability then return true end
+	if not orders or not orders.npc or not orders.ability  or not orders.target then return true end
 	
 	if not Entity.IsAbility(orders.ability) then return true end
 	if Ability.GetName(orders.ability) ~= "item_abyssal_blade" then return true end
-	
+
+	local abyssal_cast_range = 140
+	if NPC.IsEntityInRange(orders.npc, orders.target, abyssal_cast_range) then return true end
+
+	-- use blink dagger if out of range
+	local blink_cast_range = 1200
+	if NPC.HasItem(orders.npc, "item_blink", true) 
+		and NPC.IsEntityInRange(orders.npc, orders.target, blink_cast_range) then
+
+		local blink = NPC.GetItem(orders.npc, "item_blink", true)
+		local pos = NPC.GetAbsOrigin(orders.target)
+		Ability.CastPosition(blink, pos)
+		return true
+	end
 
 	return true
 end
