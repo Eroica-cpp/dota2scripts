cab45df292097629d6b1d14de40935d2a7770f2e
Tao Li <eroicacmcs@gmail.com>
2017-03-14 14:03:06 -0700

    add am's blink; fix backswing animation proble

--- AbyssalHelper.lua
+++ AbyssalHelper.lua
@@ -2,6 +2,29 @@ local AbyssalHelper = {}
 
 AbyssalHelper.option = Menu.AddOption({"Utility", "Abyssal Blade Helper"}, "Use Blink Dagger", "use blink dagger if out of range")
 
+-- use OnUpdate() to cancel animation backswing
+function AbyssalHelper.OnUpdate()
+	if not Menu.IsEnabled(AbyssalHelper.option) then return end
+
+	local myHero = Heroes.GetLocal()
+	if not myHero or NPC.IsStunned(myHero) then return end
+	
+	if not NPC.HasItem(myHero, "item_abyssal_blade", true) then return end
+	local abyssal = NPC.GetItem(myHero, "item_abyssal_blade", true)
+
+	if not Ability.IsCastable(abyssal, NPC.GetMana(myHero)) then return end
+
+	local abyssal_cast_range = 140
+	local enemies = NPC.GetHeroesInRadius(myHero, abyssal_cast_range, Enum.TeamType.TEAM_ENEMY)
+	if not enemies then return end
+
+	local target = enemies[1]
+	if target and not NPC.IsIllusion(target) and not Entity.IsDormant(target) and Entity.IsAlive(target) and not NPC.IsStunned(target) then
+		Ability.CastTarget(abyssal, target)
+	end
+
+end
+
 function AbyssalHelper.OnPrepareUnitOrders(orders)
 	if not Menu.IsEnabled(AbyssalHelper.option) then return true end
 	if not orders or not orders.npc or not orders.ability  or not orders.target then return true end
@@ -21,6 +44,23 @@ function AbyssalHelper.OnPrepareUnitOrders(orders)
 		local pos = NPC.GetAbsOrigin(orders.target)
 		Ability.CastPosition(blink, pos)
 		return true
+	end
+
+	-- anti-mage
+	if NPC.GetUnitName(myHero) ~= "npc_dota_hero_antimage" then
+		local blink = NPC.GetAbilityByIndex(orders.npc, 1)
+		local level = Ability.GetLevel(blink)
+		local cast_range = 925 + 75*(level-1)
+		local mana = NPC.GetMana(orders.npc)
+		
+		if NPC.IsEntityInRange(orders.npc, orders.target, cast_range) 
+			and Ability.IsCastable(blink, mana) then
+			
+			local pos = NPC.GetAbsOrigin(orders.target)
+			Ability.CastPosition(blink, pos)
+
+			return true
+		end
 	end
 
 	return true
