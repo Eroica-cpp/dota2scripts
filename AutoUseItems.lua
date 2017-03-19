69f99f01e1e0057c9bc0abf3f1b8cb0af8949ba5
Tao Li <eroicacmcs@gmail.com>
2017-03-18 17:13:36 -0700

    ok version for using quelling blade, iron talon or battlefury to deward

--- AutoUseItems.lua
+++ AutoUseItems.lua
@@ -1,5 +1,6 @@
 local AutoUseItems = {}
 
+AutoUseItems.optionDeward = Menu.AddOption({"Item Specific"}, "Deward", "Auto use quelling blade, iron talen, or battle fury to deward")
 AutoUseItems.optionQuellingBlade = Menu.AddOption({"Item Specific"}, "Quelling Blade", "Auto use quelling blade to deward")
 AutoUseItems.optionIronTalon = Menu.AddOption({"Item Specific"}, "Iron Talon", "Auto use iron talen to remove HP and deward")
 
@@ -8,14 +9,41 @@ function AutoUseItems.OnUpdate()
     if not myHero then return end
 
     if NPC.HasState(myHero, Enum.ModifierState.MODIFIER_STATE_INVISIBLE) then return end
+    if NPC.IsChannellingAbility(myHero) then return end
 
-    if Menu.IsEnabled(AutoUseItems.optionQuellingBlade) then
-    	AutoUseItems.item_quelling_blade(myHero)
+    if Menu.IsEnabled(AutoUseItems.optionDeward) then
+    	AutoUseItems.deward(myHero)
     end
 
-    if Menu.IsEnabled(AutoUseItems.optionIronTalon) then
-    	AutoUseItems.item_iron_talon(myHero)
-    end
+    -- if Menu.IsEnabled(AutoUseItems.optionQuellingBlade) then
+    -- 	AutoUseItems.item_quelling_blade(myHero)
+    -- end
+
+    -- if Menu.IsEnabled(AutoUseItems.optionIronTalon) then
+    -- 	AutoUseItems.item_iron_talon(myHero)
+    -- end
+
+end
+
+function AutoUseItems.deward(myHero)
+	local item1 = NPC.GetItem(myHero, "item_quelling_blade", true)
+	local item2 = NPC.GetItem(myHero, "item_iron_talon", true)
+	local item3 = NPC.GetItem(myHero, "item_bfury", true)
+
+	local item = nil
+	if item1 and Ability.IsCastable(item1, 0) then item = item1 end
+	if item2 and Ability.IsCastable(item2, 0) then item = item2 end
+	if item3 and Ability.IsCastable(item3, 0) then item = item3 end
+	if not item then return end
+
+	local range = 450
+	local wards = NPC.GetUnitsInRadius(myHero, range, Enum.TeamType.TEAM_ENEMY)
+	for i, npc in ipairs(wards) do
+		if NPC.GetUnitName(npc) == "npc_dota_observer_wards" or NPC.GetUnitName(npc) == "npc_dota_sentry_wards" then
+			Ability.CastTarget(item, npc)
+			return
+		end
+	end
 
 end
 
