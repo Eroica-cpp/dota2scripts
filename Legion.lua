c4d13f24933e16ac90312ff239e4eb87cda3cc08
Tao Li <eroicacmcs@gmail.com>
2017-03-23 15:10:38 -0700

    add function to calcualte overwhelming's damage

--- Legion.lua
+++ Legion.lua
@@ -25,16 +25,48 @@ function Legion.OverwhelmingOdds(myHero)
 	local enemies = NPC.GetUnitsInRadius(myHero, range+radius, Enum.TeamType.TEAM_ENEMY)
 	if not enemies or #enemies <= 0 then return end
 
+	local maxDamage = 0
+
 	local num = #enemies
 	for i = 1, num do
-		for j = i+1, num do
+		for j = i, num do
 			if enemies[i] and enemies[j] then
 				local vec1 = NPC.GetAbsOrigin(enemies[i])
 				local vec2 = NPC.GetAbsOrigin(enemies[j])
 				local mid = (vec1 + vec2):Scaled(0.5)
+
+				local damage = Legion.GetOverwhelmingDamage(myHero, overwhelming, mid)
+				maxDamage = math.max(damage, maxDamage)
 			end
 		end
 	end
+
+	Log.Write("maxDamage: " .. maxDamage)
+end
+
+function Legion.GetOverwhelmingDamage(myHero, overwhelming, pos)
+	if not overwhelming or not pos then return 0 end
+
+	local level = Ability.GetLevel(overwhelming)
+	if level <= 0 then return 0 end
+
+	local damage_base = 40 + 20 * (level - 1)
+	local damage_per_creep = 14 + 4 * (level - 1)
+	local damage_per_hero = 30 * level
+	
+	local res = damage_base
+
+	local radius = 330
+	local enemies = NPCs.InRadius(pos, radius, Entity.GetTeamNum(myHero), Enum.TeamType.TEAM_ENEMY)
+	for i, enemy in ipairs(enemies) do
+		if NPC.IsHero(enemy) and not NPC.IsIllusion(enemy) then
+			res = res + damage_per_hero
+		else
+			res = res + damage_per_creep
+		end
+	end
+
+	return res
 end
 
 return Legion
