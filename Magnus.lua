2ced66d7bc83f07f81518812c0facdd7e23e5580
Tao Li <eroicacmcs@gmail.com>
2017-03-28 17:54:00 -0700

    auto cast empower, ok version

--- Magnus.lua
+++ Magnus.lua
@@ -15,8 +15,37 @@ function Magnus.OnUpdate()
 	end
 end
 
+-- auto cast empower on himself or needed ally heroes
+-- priority: magnus -> melee ally hero -> ranged ally hero
 function Magnus.AutoEmpower(myHero)
-	Log.Write("yo")
+	local empower = NPC.GetAbilityByIndex(myHero, 1)
+	if not empower or not Ability.IsCastable(empower, NPC.GetMana(myHero)) then return end
+
+	if not NPC.HasModifier(myHero, "modifier_magnataur_empower") then
+		Ability.CastTarget(empower, myHero)
+		return
+	end
+
+	local range = 800
+	local alliesAround = NPC.GetHeroesInRadius(myHero, range, Enum.TeamType.TEAM_FRIEND)
+	if not alliesAround or #alliesAround <= 0 then return end
+
+	-- for melee ally hero
+	for i, ally in ipairs(alliesAround) do
+		if not NPC.HasModifier(ally, "modifier_magnataur_empower") 
+			and not NPC.IsIllusion(ally) and not NPC.IsRanged(ally) then
+			Ability.CastTarget(empower, ally)
+			return
+		end
+	end
+
+	-- for range ally hero
+	for i, ally in ipairs(alliesAround) do
+		if not NPC.HasModifier(ally, "modifier_magnataur_empower") and not NPC.IsIllusion(ally) then
+			Ability.CastTarget(empower, ally)
+			return
+		end
+	end
 end
 
 return Magnus
