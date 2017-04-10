2ce9808a91b6d940d900e48c9096dbe42aef50bb
Tao Li <eroicacmcs@gmail.com>
2017-04-10 16:57:21 -0700

    draw on new mini map, ok version

--- Draw.lua
+++ Draw.lua
@@ -1,9 +1,9 @@
 local Draw = {}
 
 -- draw a new map right next to build-in map
--- new map position: (230, 630) -> (370, 765)
-local map_size = 120
-local map_origin = Vector(230, 630)
+-- new map position: (238, 606) -> (370, 765)
+local map_size = 160
+local map_origin = Vector(238, 606)
 
 local path = "resource/flash3/images/miniheroes/"
 local cache = {}
@@ -12,11 +12,12 @@ function Draw.OnDraw()
     local myHero = Heroes.GetLocal()
     if not myHero then return end
 
-    local name = "npc_dota_hero_lina"
+    local name = "npc_dota_hero_invoker"
     local pos = Entity.GetAbsOrigin(myHero)
+
+    -- Draw.DrawMap()
     -- Draw.DrawHero(name, pos)
 end
-
 
 -- draw hero's icon on map or ground
 function Draw.DrawHero(heroName, pos)
@@ -29,17 +30,20 @@ function Draw.DrawHero(heroName, pos)
         cache[heroName] = handler
     end
 
-    local size = 50
     Renderer.SetDrawColor(255, 255, 255, 255)
+    
+    -- draw on the ground
+    local size = 50
     local x, y, visible = Renderer.WorldToScreen(pos)
     Renderer.DrawImage(handler, x-math.floor(size/2), y-math.floor(size/2), size, size)
 
+    -- draw on new mini map
+    local size = 28
     local x, y = Draw.WorldToMap(pos)
-    Draw.DrawOnMap(handler, x, y)
-
+    Renderer.DrawImage(handler, x-size/2, y-size/2, size, size)
 end
 
-function Draw.DrawOnMap(handler, x, y)
+function Draw.DrawMap()
     if Input.IsCursorInRect(map_origin:GetX(), map_origin:GetY(), map_size, map_size) and Input.IsKeyDown(Enum.ButtonCode.KEY_LALT) then
         local cursor_x, cursor_y = Renderer.WorldToScreen(Input.GetWorldCursorPos())
         map_origin = Vector(cursor_x - map_size/2, cursor_y - map_size/2)
@@ -47,14 +51,10 @@ function Draw.DrawOnMap(handler, x, y)
 
     Renderer.SetDrawColor(255, 255, 255, 255)
     Renderer.DrawFilledRect(map_origin:GetX(), map_origin:GetY(), map_size, map_size)
-
-    if not handler or not x or not y then return end
-    Renderer.DrawImage(handler, x, y)
 end
 
 -- map: position in world -> position in map (screen)
--- origin in my screen: (6, 575) -> (198, 755)
--- mini word origin: (-3220, -2842) -> (4222, 2838)
+-- mini world origin: (-3220, -2842) -> (4222, 2838)
 -- world origin: (-7600, -7400) -> (8000, 7400)
 function Draw.WorldToMap(pos)
     local world_origin = Vector(-7600, -7400)
@@ -63,8 +63,12 @@ function Draw.WorldToMap(pos)
     local world_len = (world_end - world_origin):Length()
     local map_len = map_size * math.sqrt(2)
 
-    local map_pos = map_origin + (pos - world_origin):Scaled(map_len/world_len)
-    return map_pos:GetX(), map_pos:GetY()
+    local dir = (pos - world_origin):Scaled(map_len/world_len)
+
+    local map_x = map_origin:GetX() + dir:GetX()
+    local map_y = map_origin:GetY() + (map_size - dir:GetY())
+
+    return math.floor(map_x), math.floor(map_y)
 end
 
 return Draw
