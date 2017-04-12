8c76600f205f77c87400dca9574cf5f89d483b15
Tao Li <eroicacmcs@gmail.com>
2017-04-12 16:24:33 -0700

    add ultimate helper

--- Puck.lua
+++ Puck.lua
@@ -2,6 +2,38 @@ local Utility = require("Utility")
 
 local Puck = {}
 
+local optionUltimateHelper = Menu.AddOption({"Hero Specific", "Puck"}, "Ultimate Helper", "Cast ultimate on best position once the order key is pressed")
+
+function Puck.OnPrepareUnitOrders(orders)
+    if not orders then return true end
+
+    if Menu.IsEnabled(optionUltimateHelper) and orders.ability and Entity.IsAbility(orders.ability)
+    	and Ability.GetName(orders.ability) == "puck_dream_coil"
+    	and orders.order ~= Enum.UnitOrder.DOTA_UNIT_ORDER_TRAIN_ABILITY then
+    	
+    	Puck.UltimateHelper(orders.position)
+    	return false
+    end
+
+    return true
+end
+
+function Puck.UltimateHelper(default_pos)
+	local myHero = Heroes.GetLocal()
+	if not myHero then return end
+
+	local ultimate = NPC.GetAbility(myHero, "puck_dream_coil")
+	if not ultimate or not Ability.IsCastable(ultimate, NPC.GetMana(myHero)) then return end
+
+	local range = 750
+	local radius = 375
+	local enemyHeroes = NPC.GetHeroesInRadius(myHero, range, Enum.TeamType.TEAM_ENEMY)
+	local best_pos = Utility.BestPosition(enemyHeroes, radius)
+
+	if not best_pos and default_pos then Ability.CastPosition(ultimate, default_pos) end
+	if best_pos then Ability.CastPosition(ultimate, best_pos) end
+end
+
 -- define Puck's defind behavior
 -- input: target entity, can be nil
 function Puck.Defend(target)
