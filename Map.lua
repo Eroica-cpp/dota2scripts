94749f9d545e5770592a6aa42dfd9b459845ac1e
Tao Li <eroicacmcs@gmail.com>
2017-04-13 00:19:34 -0700

    add Map.lua, a new module to handle position info

---
+++ Map.lua
@@ -0,0 +1,45 @@
+local Map = {}
+
+Map.BuildingLocation = {
+	radiant_fountain = Vector(-7600, -7300, 640),
+	dire_fountain = Vector(7800, 7250, 640)
+}
+
+Map.CampLocation = {
+	radiant_ancient_camp_1 = Vector(-2700, -250, 384),
+	radiant_ancient_camp_2 = Vector(150, -2000, 384),
+	dire_ancient_camp_1 = Vector(-700, 2300, 384),
+	dire_ancient_camp_2 = Vector(3600, -700, 256),
+	radiant_small_camp = Vector(3250, -4500, 256),
+	dire_small_camp = Vector(-3050, 4800, 384),
+	radiant_mid_camp_1 = Vector(-3900, 600, 256),
+	radiant_mid_camp_2 = Vector(-1800, 4150, 128),
+	radiant_mid_camp_3 = Vector(650, -4600, 384),
+	dire_mid_camp_1 = Vector(-1650, 4000, 256),
+	dire_mid_camp_2 = Vector(1100, 3500, 384),
+	dire_mid_camp_3 = Vector(2800, 100, 384),
+	radiant_large_camp_1 = Vector(-4700, -350, 256),
+	radiant_large_camp_2 = Vector(-600, -3300, 256),
+	radiant_large_camp_3 = Vector(4500, -4300, 256),
+	dire_large_camp_1 = Vector(-4350, 3700, 256),
+	dire_large_camp_2 = Vector(-300, 3400, 256),
+	dire_large_camp_3 = Vector(4350, 750, 384)
+}
+
+function Map.InFountain(pos)
+	local range = 2000
+	if (Map.BuildingLocation["radiant_fountain"] - pos):Length() <= range then return true end
+	if (Map.BuildingLocation["dire_fountain"] - pos):Length() <= range then return true end
+	return false
+end
+
+function Map.InNeutralCamp(pos)
+	local range = 600
+	for name, location in pairs(Map.CampLocation) do
+		local dis = (location - pos):Length()
+		if dis <= range then return true end
+	end
+	return false
+end
+
+return Map
