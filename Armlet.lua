682526bba75b9aa1ba365c74d2b21c0d1fd112fc
Tao Li <eroicacmcs@gmail.com>
2017-04-26 15:19:16 -0700

    try to auto toggle when hp is low, TBD

--- Armlet.lua
+++ Armlet.lua
@@ -1,8 +1,11 @@
+local Utility = require("Utility")
+
 local Armlet = {}
 
 local option = Menu.AddOption({"Item Specific"}, "Armlet", "Auto toggle armlet")
 
-local HpThreshold = 550
+local safeThreshold = 550
+local dangerousThreshold = 200
 
 function Armlet.OnPrepareUnitOrders(orders)
     if not Menu.IsEnabled(option) then return true end
@@ -10,6 +13,7 @@ function Armlet.OnPrepareUnitOrders(orders)
 
     local myHero = Heroes.GetLocal()
     if not myHero then return true end
+    if not Utility.IsSuitableToUseItem(myHero) then return true end
 
     local item = NPC.GetItem(myHero, "item_armlet", true)
     if not item then return true end
@@ -20,11 +24,35 @@ function Armlet.OnPrepareUnitOrders(orders)
     end
 
     -- toggle off armlet if about to walk
-    if Ability.GetToggleState(item) and Entity.GetHealth(myHero) >= HpThreshold and (orders.order == Enum.UnitOrder.DOTA_UNIT_ORDER_MOVE_TO_POSITION or orders.order == Enum.UnitOrder.DOTA_UNIT_ORDER_MOVE_TO_TARGET) then
+    if Ability.GetToggleState(item) and Entity.GetHealth(myHero) >= safeThreshold and (orders.order == Enum.UnitOrder.DOTA_UNIT_ORDER_MOVE_TO_POSITION or orders.order == Enum.UnitOrder.DOTA_UNIT_ORDER_MOVE_TO_TARGET) then
         Ability.Toggle(item)
     end
 
     return true
 end
+
+-- function Armlet.OnUpdate()
+--     if not Menu.IsEnabled(option) then return end
+
+--     local myHero = Heroes.GetLocal()
+--     if not myHero then return end
+
+--     local item = NPC.GetItem(myHero, "item_armlet", true)
+--     if not item then return end
+
+--     local delay = 0.6
+--     local mod = NPC.GetModifier(myHero, "modifier_item_armlet_unholy_strength")
+
+--     if Entity.GetHealth(myHero) <= dangerousThreshold then
+--         -- if doesn't toggle, then toggle armlet
+--         if not mod then Ability.Toggle(item); return end
+        
+--         -- if does toggled, toggle it again after 0.6 second
+--         if math.abs(GameRules.GetGameTime() - Modifier.GetCreationTime(mod) - delay) <= 0.05 then
+--             Ability.Toggle(item)
+--             Ability.Toggle(item)
+--         end
+--     end
+-- end
 
 return Armlet
