2e6880d4fb7fe94051af3dc443fee274a6f8715a
Tao Li <eroicacmcs@gmail.com>
2017-06-09 17:26:59 -0700

    add auto goo

---
+++ Bristleback.lua
@@ -0,0 +1,38 @@
+-- File: Bristleback.lua
+-- Author: EroicaCpp (https://eroicacpp.com/dota2/)
+-- Version: 1.0
+-- Date: June 9, 2017
+
+local Utility = require("Utility")
+
+local Bristleback = {}
+
+local optionAutoGoo = Menu.AddOption({"Hero Specific", "Bristleback"}, "Auto Goo", "Auto cast Goo once a enemy is in range")
+local optionAutoQuill = Menu.AddOption({"Hero Specific", "Bristleback"}, "Auto Quill", "Auto cast Quill once enemies are in range")
+local key = Menu.AddKeyOption({"Hero Specific", "Bristleback"}, "Activate Auto Spells Key", Enum.ButtonCode.KEY_E)
+local font = Renderer.LoadFont("Tahoma", 24, Enum.FontWeight.EXTRABOLD)
+
+
+function Bristleback.OnUpdate()
+    local myHero = Heroes.GetLocal()
+    if not myHero or not NPC.GetUnitName(myHero) ~= "npc_dota_hero_bristleback" then return end
+
+    if Menu.IsEnabled(optionAutoGoo) then
+        Bristleback.AutoGoo(myHero)
+    end
+end
+
+function Bristleback.AutoGoo(myHero)
+    local goo = NPC.GetAbility(myHero, "bristleback_viscous_nasal_goo")
+    if not goo or not Ability.IsCastable(goo, NPC.GetMana(myHero)) then return end
+
+    local range = 600
+    local enemies = NPC.GetUnitsInRadius(myHero, range, Enum.TeamType.TEAM_ENEMY)
+    for i, npc in ipairs(enemies) do
+        if not NPC.IsIllusion(npc) and Utility.CanCastSpellOn(npc) and not Utility.IsLotusProtected(npc) then
+            Ability.CastTarget(goo, npc)
+        end
+    end
+end
+
+return Bristleback
