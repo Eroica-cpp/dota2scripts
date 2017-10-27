5a76cdb094496f5f8900097c9b66d32feeb55ddf
Tao Li <eroicacmcs@gmail.com>
2017-10-27 00:21:29 -0700

    update indent

--- Rubick.lua
+++ Rubick.lua
@@ -5,17 +5,17 @@ local Rubick = {}
 local optionKillSteal = Menu.AddOption({"Hero Specific", "Rubick"}, "Kill Steal", "Cast spell on enemy to KS")
 
 function Rubick.OnUpdate()
-	local myHero = Heroes.GetLocal()
-	if not myHero then return end
+    local myHero = Heroes.GetLocal()
+    if not myHero then return end
 
-	if Menu.IsEnabled(optionKillSteal) then
-		Rubick.KillSteal()
-	end
+    if Menu.IsEnabled(optionKillSteal) then
+        Rubick.KillSteal()
+    end
 end
 
 function Rubick.KillSteal()
     local myHero = Heroes.GetLocal()
-	if not myHero then return end
+    if not myHero then return end
 
     local bolt = NPC.GetAbility(myHero, "rubick_fade_bolt")
     if not bolt or not Ability.IsCastable(bolt, NPC.GetMana(myHero)) then return end
@@ -24,17 +24,17 @@ function Rubick.KillSteal()
     local damage = 80 * Ability.GetLevel(bolt)
 
     for i = 1, Heroes.Count() do
-		local enemy = Heroes.Get(i)
-		if enemy and not NPC.IsIllusion(enemy) and not Entity.IsSameTeam(myHero, enemy)
+        local enemy = Heroes.Get(i)
+        if enemy and not NPC.IsIllusion(enemy) and not Entity.IsSameTeam(myHero, enemy)
         and Utility.CanCastSpellOn(enemy) and NPC.IsEntityInRange(myHero, enemy, range) then
 
-			local true_damage = damage * NPC.GetMagicalArmorDamageMultiplier(enemy)
-			if true_damage >= Entity.GetHealth(enemy) then
+            local true_damage = damage * NPC.GetMagicalArmorDamageMultiplier(enemy)
+            if true_damage >= Entity.GetHealth(enemy) then
                 Ability.CastTarget(bolt, enemy)
                 return
             end
-		end
-	end
+        end
+    end
 end
 
 return Rubick
