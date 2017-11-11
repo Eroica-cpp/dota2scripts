28c9472085b97ad3915ebda52a912eb6ef70d6cf
Tao Li <eroicacmcs@gmail.com>
2017-11-10 16:02:12 -0800

    add spike helper

--- Lion.lua
+++ Lion.lua
@@ -3,10 +3,15 @@ local Utility = require("Utility")
 local Lion = {}
 
 local optionAutoHex = Menu.AddOption({"Hero Specific", "Lion"}, "Auto Hex", "Auto hex any enemy in range once lion has level 6")
+local optionAutoSpike = Menu.AddOption({"Hero Specific", "Lion"}, "Auto Spike", "Auto spike if enemy (1) is TPing; (2) channelling; or (3) being stunned or hexed with proper timing")
 
 function Lion.OnUpdate()
     if Menu.IsEnabled(optionAutoHex) then
         Lion.AutoHex()
+    end
+
+    if Menu.IsEnabled(optionAutoSpike) then
+        Lion.AutoSpike()
     end
 end
 
@@ -26,6 +31,38 @@ function Lion.AutoHex()
 
             Ability.CastTarget(spell, enemy)
             return
+        end
+    end
+end
+
+function Lion.AutoSpike()
+    local myHero = Heroes.GetLocal()
+    if not myHero or not Utility.IsSuitableToCastSpell(myHero) then return end
+
+    local spell = NPC.GetAbility(myHero, "lion_impale")
+    if not spell or not Ability.IsCastable(spell, NPC.GetMana(myHero)) then return end
+    local range = Ability.GetCastRange(spell)
+    local speed = 1600
+
+    for i = 1, Heroes.Count() do
+        local enemy = Heroes.Get(i)
+        if enemy and not NPC.IsIllusion(enemy) and not Entity.IsSameTeam(myHero, enemy)
+        and Utility.CanCastSpellOn(enemy) and NPC.IsEntityInRange(myHero, enemy, range) then
+
+            -- spike the enemy who is channelling a spell or TPing
+            if Utility.IsChannellingAbility(enemy) then
+                Ability.CastPosition(spell, Entity.GetAbsOrigin(enemy))
+                return
+            end
+
+            -- spike the enemy who is being stunned or hexed with proper timing
+            local dis = (Entity.GetAbsOrigin(myHero) - Entity.GetAbsOrigin(enemy)):Length()
+            local delay = 0.3 + dis/speed
+            if (Utility.GetHexTimeLeft(enemy) - 0.1 < delay and delay < Utility.GetHexTimeLeft(enemy) + 0.1)
+            or (Utility.GetStunTimeLeft(enemy) - 0.1 < delay and delay < Utility.GetStunTimeLeft(enemy) + 0.1) then
+                Ability.CastPosition(spell, Utility.GetPredictedPosition(enemy, delay))
+                return
+            end
         end
     end
 end
