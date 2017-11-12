83c9feb50583297b754114327c540d1b806c826b
Tao Li <eroicacmcs@gmail.com>
2017-11-12 15:58:11 -0800

    add autoManaDrain; rm ManaDrainHelper()

--- Lion.lua
+++ Lion.lua
@@ -4,34 +4,7 @@ local Lion = {}
 
 local optionAutoHex = Menu.AddOption({"Hero Specific", "Lion"}, "Auto Hex", "Auto hex any enemy in range once lion has level 6")
 local optionAutoSpike = Menu.AddOption({"Hero Specific", "Lion"}, "Auto Spike", "Auto spike if enemy (1) is TPing; (2) channelling; or (3) being stunned or hexed with proper timing")
-local optionManaDrainHelper = Menu.AddOption({"Hero Specific", "Lion"}, "Mana Drain Helper", "Help auto select target(s) for mana drain")
-
-function Lion.OnPrepareUnitOrders(orders)
-
-    if not Menu.IsEnabled(optionManaDrainHelper) then return true end
-    if not orders or not orders.ability then return true end
-    if orders.order == Enum.UnitOrder.DOTA_UNIT_ORDER_TRAIN_ABILITY then return true end
-
-    if not Entity.IsAbility(orders.ability) then return true end
-    if Ability.GetName(orders.ability) ~= "lion_mana_drain" then return true end
-
-    local myHero = Heroes.GetLocal()
-    if not myHero then return true end
-
-    local range = Ability.GetCastRange(orders.ability)
-
-    for i = 1, Heroes.Count() do
-        local enemy = Heroes.Get(i)
-        if enemy and not Entity.IsSameTeam(myHero, enemy)
-        and Utility.CanCastSpellOn(enemy) and NPC.IsEntityInRange(myHero, enemy, range) then
-
-            Ability.CastTarget(orders.ability, enemy)
-            return false
-        end
-    end
-
-    return true
-end
+local optionAutoManaDrain = Menu.AddOption({"Hero Specific", "Lion"}, "Auto Mana Drain", "Auto mana drain (1) stuned/hexed/taunted enemy; (2) illusion")
 
 function Lion.OnUpdate()
     if Menu.IsEnabled(optionAutoHex) then
@@ -40,6 +13,10 @@ function Lion.OnUpdate()
 
     if Menu.IsEnabled(optionAutoSpike) then
         Lion.AutoSpike()
+    end
+
+    if Menu.IsEnabled(optionAutoManaDrain) then
+        Lion.AutoManaDrain()
     end
 end
 
@@ -90,6 +67,27 @@ function Lion.AutoSpike()
             if (Utility.GetHexTimeLeft(enemy) - 0.1 < delay and delay < Utility.GetHexTimeLeft(enemy) + 0.1)
             or (Utility.GetStunTimeLeft(enemy) - 0.1 < delay and delay < Utility.GetStunTimeLeft(enemy) + 0.1) then
                 Ability.CastPosition(spell, Utility.GetPredictedPosition(enemy, delay))
+                return
+            end
+        end
+    end
+end
+
+function Lion.AutoManaDrain()
+    local myHero = Heroes.GetLocal()
+    if not myHero or not Lion.IsSuitableToCastSpell(myHero) then return end
+
+    local spell = NPC.GetAbility(myHero, "lion_mana_drain")
+    if not spell or not Ability.IsCastable(spell, NPC.GetMana(myHero)) then return end
+    local range = Ability.GetCastRange(spell)
+
+    for i = 1, Heroes.Count() do
+        local enemy = Heroes.Get(i)
+        if enemy and not Entity.IsSameTeam(myHero, enemy)
+        and Utility.CanCastSpellOn(enemy) and NPC.IsEntityInRange(myHero, enemy, range) then
+
+            if NPC.IsIllusion(enemy) or Utility.CantMove(enemy) or Utility.IsDisabled(enemy) then
+                Ability.CastTarget(spell, enemy)
                 return
             end
         end
