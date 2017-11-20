ffd913518a6279206279d7f88f638101e3607ba1
Tao Li <eroicacmcs@gmail.com>
2017-11-19 18:11:13 -0800

    add particle effect notifier (miranr, smoke, and nyx)

--- Notification.lua
+++ Notification.lua
@@ -4,6 +4,20 @@ local Map = require("Map")
 local Notification = {}
 
 local optionRoshan = Menu.AddOption({"Awareness", "Notification"}, "Roshan", "Notify teammates (print on the screen) when someone's roshing.")
+local optionParticleEffect = Menu.AddOption({"Awareness", "Notification"}, "Particle Effect", "Notify teammates (print on the screen) when certain particle effects happen.")
+
+local ParticleEffectList = {
+    mirana_moonlight_recipient = "Mirana has used her ultimate.",
+    smoke_of_deceit = "Smoke of Deceit has been used.",
+    nyx_assassin_vendetta_start = "Nyx has used Vendetta."
+}
+
+function Notification.OnParticleCreate(particle)
+    if not particle then return end
+    if Menu.IsEnabled(optionParticleEffect) and particle.name and ParticleEffectList[particle.name] then
+        Chat.Say("DOTAChannelType_GameAllies", ParticleEffectList[particle.name])
+    end
+end
 
 function Notification.OnParticleUpdate(particle)
     if not particle or not particle.index then return end
