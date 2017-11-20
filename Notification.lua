b46a8a5f4b0acc913408e197d3794902356fcc63
Tao Li <eroicacmcs@gmail.com>
2017-11-19 17:08:38 -0800

    use two particle effect detectors

--- Notification.lua
+++ Notification.lua
@@ -5,14 +5,6 @@ local Notification = {}
 
 local optionRoshan = Menu.AddOption({"Awareness", "Notification"}, "Roshan", "Notify teammates (print on the screen) when someone's roshing.")
 
--- function Notification.OnUpdate()
---     if not Menu.IsEnabled(optionRoshan) then return end
---
---     local myHero = Heroes.GetLocal()
---     if not myHero then return end
---     Chat.Say("DOTAChannelType_GameAllies", "dota wtf")
--- end
-
 function Notification.OnParticleUpdate(particle)
     if not particle or not particle.index then return end
     if not particle.position or not Map.IsValidPos(particle.position) then return end
@@ -22,14 +14,13 @@ function Notification.OnParticleUpdate(particle)
     end
 end
 
--- function Notification.OnParticleUpdateEntity(particle)
---     if not particle then return end
---     if not particle.entity or not NPC.IsHero(particle.entity) then return end
---     if not particle.position or not Map.IsValidPos(particle.position) then return end
---
---     if Menu.IsEnabled(optionRoshan) and Map.InRoshan(particle.position) then
---         Chat.Say("DOTAChannelType_GameAllies", "Someone is roshing!")
---     end
--- end
+function Notification.OnParticleUpdateEntity(particle)
+    if not particle then return end
+    if not particle.position or not Map.IsValidPos(particle.position) then return end
+
+    if Menu.IsEnabled(optionRoshan) and Map.InRoshan(particle.position) then
+        Chat.Say("DOTAChannelType_GameAllies", "Someone is roshing!")
+    end
+end
 
 return Notification
