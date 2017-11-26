b2653f1f4925c981980020ec2d25a14d9a241970
Tao Li <eroicacmcs@gmail.com>
2017-11-25 22:45:55 -0800

    use Chinese notification

--- Notification.lua
+++ Notification.lua
@@ -7,9 +7,9 @@ local optionRoshan = Menu.AddOption({"Awareness", "Notification"}, "Roshan", "No
 local optionParticleEffect = Menu.AddOption({"Awareness", "Notification"}, "Particle Effect", "Notify teammates (print on the screen) when certain particle effects happen.")
 
 local ParticleEffectList = {
-    mirana_moonlight_recipient = "Mirana has used her ultimate.",
-    smoke_of_deceit = "Smoke of Deceit has been used.",
-    nyx_assassin_vendetta_start = "Nyx has used Vendetta."
+    mirana_moonlight_recipient = "pom开大了！",
+    smoke_of_deceit = "开雾了！",
+    nyx_assassin_vendetta_start = "小强隐身了！"
 }
 
 function Notification.OnParticleCreate(particle)
@@ -24,7 +24,8 @@ function Notification.OnParticleUpdate(particle)
     if not particle.position or not Map.IsValidPos(particle.position) then return end
 
     if Menu.IsEnabled(optionRoshan) and Map.InRoshan(particle.position) then
-        Chat.Say("DOTAChannelType_GameAllies", "Someone is roshing!")
+        -- Chat.Say("DOTAChannelType_GameAllies", "Someone is roshing!")
+        Chat.Say("DOTAChannelType_GameAllies", "有人在打盾！")
     end
 end
 
@@ -33,7 +34,8 @@ function Notification.OnParticleUpdateEntity(particle)
     if not particle.position or not Map.IsValidPos(particle.position) then return end
 
     if Menu.IsEnabled(optionRoshan) and Map.InRoshan(particle.position) then
-        Chat.Say("DOTAChannelType_GameAllies", "Someone is roshing!")
+        -- Chat.Say("DOTAChannelType_GameAllies", "Someone is roshing!")
+        Chat.Say("DOTAChannelType_GameAllies", "有人在打盾！")
     end
 end
 
