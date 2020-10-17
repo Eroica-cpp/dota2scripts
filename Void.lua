baba6a2bc14e468eddbad8762d22726e29ef3033
Tao Li <taoli.cpp@gmail.com>
2020-10-17 13:35:41 -0400

    add hits count down for void, ok version

--- Void.lua
+++ Void.lua
@@ -11,21 +11,32 @@ function Void.OnDraw()
     if not myHero or NPC.GetUnitName(myHero) ~= "npc_dota_hero_faceless_void" then return end
     -- if NPC.GetCurrentLevel(myHero) < 6 then return end
 
-    local spell = NPC.GetAbility(myHero, "faceless_void_time_lock")
+    local faceless_void_time_lock = NPC.GetAbility(myHero, "faceless_void_time_lock")
     local prob = 0
     local magicalDamage = 0
-    if spell and Ability.GetLevel(spell) >= 1 then
-	    prob = 0.08 + 0.04 * Ability.GetLevel(spell)
-	    magicalDamage = 15 + 5 * Ability.GetLevel(spell)
+    if faceless_void_time_lock and Ability.GetLevel(faceless_void_time_lock) >= 1 then
+	    prob = 0.08 + 0.04 * Ability.GetLevel(faceless_void_time_lock)
+	    magicalDamage = 15 + 5 * Ability.GetLevel(faceless_void_time_lock)
 	end
+
+	-- the expectation of extra hits:
+	-- expectation = prob + prob**2 + ... + prob**n, (n to \infty)
+	expectation = prob / (1 - prob)
+
+	local faceless_void_chronosphere = NPC.GetAbility(myHero, "faceless_void_chronosphere")
+	local duration = 0
+	if faceless_void_chronosphere and Ability.GetLevel(faceless_void_chronosphere) >= 1 then
+		duration = 3.5 + 0.5 * Ability.GetLevel(faceless_void_time_lock)
+	end
+	local attacksPerChrono = math.floor(duration * NPC.GetAttacksPerSecond(myHero))
 
     for i = 1, Heroes.Count() do
         local enemy = Heroes.Get(i)
         if not NPC.IsIllusion(enemy) and not Entity.IsSameTeam(myHero, enemy)
             and not Entity.IsDormant(enemy) and Entity.IsAlive(enemy) then
 
-            local oneHitMagicalDamage = prob * Utility.GetRealDamage(myHero, enemy, magicalDamage)
-            local oneHitPhysicalDamage = (1 + prob) * NPC.GetTrueDamage(myHero) * NPC.GetArmorDamageMultiplier(enemy)
+            local oneHitMagicalDamage = expectation * Utility.GetRealDamage(myHero, enemy, magicalDamage)
+            local oneHitPhysicalDamage = (1 + expectation) * NPC.GetTrueDamage(myHero) * NPC.GetArmorDamageMultiplier(enemy)
             local oneHitDamage = oneHitMagicalDamage + oneHitPhysicalDamage
             local hitsLeft = math.ceil(Entity.GetHealth(enemy) / oneHitDamage)
 
@@ -34,13 +45,14 @@ function Void.OnDraw()
             local x, y, visible = Renderer.WorldToScreen(pos)
             local font = Renderer.LoadFont("Tahoma", 30, Enum.FontWeight.EXTRABOLD)
 
+            text = string.format("%d (%d)", hitsLeft, attacksPerChrono)
             -- red : can kill; green : cant kill
-            if hitsLeft <= 0 then
+            if hitsLeft <= attacksPerChrono then
                 Renderer.SetDrawColor(255, 0, 0, 255)
-                Renderer.DrawTextCentered(font, x, y, "Kill", 1)
+                Renderer.DrawTextCentered(font, x, y, text, 1)
             else
                 Renderer.SetDrawColor(0, 255, 0, 255)
-                Renderer.DrawTextCentered(font, x, y, hitsLeft, 1)
+                Renderer.DrawTextCentered(font, x, y, text, 1)
             end
 
         end
